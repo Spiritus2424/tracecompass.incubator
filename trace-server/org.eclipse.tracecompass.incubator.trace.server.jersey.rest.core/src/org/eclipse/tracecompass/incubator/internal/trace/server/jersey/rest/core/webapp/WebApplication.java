ec20fbb8155c4ef211afef9d8d0b2d9714845d67
Ahmad Faour <ahmad.faour@polymtl.ca>
2023-07-18 18:14:15 -0400

    register controller action
    Change-Id: I2185273e520cb6c77fd6d6f4e683b922e6d1881d

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/webapp/WebApplication.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/webapp/WebApplication.java
@@ -142,6 +142,7 @@ public class WebApplication {
         rc.register(DataProviderService.class);
         rc.register(FilterService.class);
         rc.register(HealthService.class);
+        rc.register(XmlService.class);
         rc.register(CORSFilter.class);
         rc.register(JacksonObjectMapperProvider.class);
         EncodingFilter.enableFor(rc, GZipEncoder.class);
