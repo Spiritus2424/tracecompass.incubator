acca5df1b386dbdfa00135b0106ebae59af167f2
Ahmad Faour <ahmad.faour@polymtl.ca>
2023-11-05 15:55:23 -0500

    WIP
    Change-Id: Ia473cc0b0b80090ee9d7846582da2e30eeae2a2f

--- trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/CreateCriticalPathDto.java
+++ trace-server/org.eclipse.tracecompass.incubator.trace.server.jersey.rest.core/src/org/eclipse/tracecompass/incubator/internal/trace/server/jersey/rest/core/services/CreateCriticalPathDto.java
@@ -1,8 +1,6 @@
 package org.eclipse.tracecompass.incubator.internal.trace.server.jersey.rest.core.services;
 
-import org.eclipse.tracecompass.internal.analysis.graph.core.graph.historytree.TmfVertex;
-
 public class CreateCriticalPathDto {
-    public TmfVertex startVertex;
-    public TmfVertex endVertex;
+    public VertexDto startVertex;
+    public VertexDto endVertex;
 }
